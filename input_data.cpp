49ec9e3956cd86a638467f3c541d5967719b71c8
raayiii <raayiii18@gmail.com>
2025-09-17 01:47:25 +0700

    Add files via upload

---
+++ input_data.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int angka;
+	float desimal;
+	char karakter;
+	cout << "Masukkan sebuah angka: ";
+	cin >> angka;
+	cout << "Masukkan sebuah angka desimal: ";
+	cin >> desimal;
+	cout << "Masukkan sebuah karakter: ";
+	cin >> karakter;
+	
+	cout << "Angka yang Anda masukkan: " << angka << endl;
+	cout << "Desimal yang Anda masukkan: " << desimal << endl;
+	cout << "Karakter yang Anda masukkan: " << karakter << endl;
+}
+
