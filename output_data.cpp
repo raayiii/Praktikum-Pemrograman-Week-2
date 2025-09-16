1b27eb114146ba7127b849fb7fbd33e25c278917
raayiii <raayiii18@gmail.com>
2025-09-17 02:42:46 +0700

    Add files via upload

---
+++ output_data.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int a = 10, b = 20;
+	float c = 3.14;
+	cout << "Nilai a: " << a << endl; 
+	 cout << "Nilai b: " << b << endl;
+	 cout << "Nilai c: " << c << endl;
+	return 0;
+}
