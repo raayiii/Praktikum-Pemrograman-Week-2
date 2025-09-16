49086d98f20dbb2b10a7ea5f517ccb0b8cdacb0b
raayiii <raayiii18@gmail.com>
2025-09-17 02:42:58 +0700

    Add files via upload

---
+++ output_rata.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main () {
+	int num1 = 2000, num2 = 39800, num3 = 489688;
+	cout << right;
+	cout << setw(10) << num1 << endl;
+	cout << setw(10) << num2 << endl;
+	cout << setw(10) << num3 << endl;
+	return 0;
+}
